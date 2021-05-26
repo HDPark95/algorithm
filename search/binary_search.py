a99f77dceb39ecb87eff4f8a8c3996f0a2978514
박현두 <qkrgusen456@gmail.com>
2021-05-26 10:02:24 +0900

    이것이 코딩테스트다 이진탐색 학습

--- search/binary_search.py
+++ search/binary_search.py
@@ -5,7 +5,7 @@
 """
 
 #재귀 함수를 이용한 case
-def binary_search(array, target, start, end):
+def binary_search_by_recursive(array, target, start, end):
     if start > end:
         return None
     mid = (start + end) // 2
@@ -14,10 +14,10 @@ def binary_search(array, target, start, end):
         return mid
     #중간점의 값보다 찾고자하는 값이 작은 경우 왼쪽 확인
     elif array[mid] > target:
-        return binary_search(array, target, start, mid -1)
+        return binary_search_by_recursive(array, target, start, mid -1)
     #중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
     else:
-        return binary_search(array, target, mid+1, end)
+        return binary_search_by_recursive(array, target, mid+1, end)
 
 # n과 target을 받기
 n, target = list(map(int, input().split()))
@@ -25,8 +25,23 @@ n, target = list(map(int, input().split()))
 array = list(map(int, input().split()))
 
 #이진 탐색 수행 결과 출력
-result = binary_search(array, target, 0, n-1)
+result = binary_search_by_recursive(array, target, 0, n-1)
 if result == None:
     print("원소가 존재하지 않습니다.")
 else:
     print(result + 1)
+
+#TODO 단순 반복문을 활용한 이진탐색
+def binary_search_by_for(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        #찾은 경우 중간점 인덳 ㅡ반환
+        if array[mid] == target:
+            return mid
+        #중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid -1
+        else:
+            start = mid + 1
+    return None
+
